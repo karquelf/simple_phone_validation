685f85399cc41cdea0145903a8721b3eac8e34e2
Fabien Karquel <fabien.karquel@gmail.com>
2018-07-11 22:15:31 +0200

    rails engine for locales

--- lib/simple_phone_validation.rb
+++ lib/simple_phone_validation.rb
@@ -1,10 +1,8 @@
 require 'simple_phone_validation/railtie'
 require 'simple_phone_validation/phone_validator'
 
-I18n.load_path += Dir.glob( File.dirname(__FILE__) + "config/locales/*.{rb,yml}" )
-
 module SimplePhoneValidation
-  # class Engine < Rails::Engine
-  # end
+  class Engine < Rails::Engine
+  end
   # Your code goes here...
 end
