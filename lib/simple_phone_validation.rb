9671619b1afbe73854d8df2d24c97394c1a2906b
Fabien Karquel <fabien.karquel@gmail.com>
2018-07-11 22:08:30 +0200

    rails_engine to load locales

--- lib/simple_phone_validation.rb
+++ lib/simple_phone_validation.rb
@@ -2,5 +2,7 @@ require 'simple_phone_validation/railtie'
 require 'simple_phone_validation/phone_validator'
 
 module SimplePhoneValidation
+  class Engine < Rails::Engine
+  end
   # Your code goes here...
 end
